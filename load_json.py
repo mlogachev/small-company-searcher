a900f07fe4356e6d9dcd0b36045786da1d5b26fd
Mikhail Logachev <miha.loga@gmail.com>
2018-10-27 16:31:15 +0300

    change

--- load_json.py
+++ load_json.py
@@ -16,24 +16,9 @@ def import_json(absolute_json_file_path, database, user):
     with psycopg2.connect(**dict(database=database, user=user)) as conn:
         with conn.cursor() as cur:
             for row in data:
-
-                if row['ParentId'] is not None:
-                    cur.execute(
-                        'SELECT OfficeId FROM employers WHERE id=%s',
-                        (row['ParentId'],)
-                    )
-                    row['OfficeId'] = cur.fetchone()[0]
-
-                    cur.execute(
-                        'UPDATE employers SET person = FALSE WHERE id = %s',
-                        (row['ParentId'],)
-                    )
-                else:
-                    row['OfficeId'] = row['id']
-
                 cur.execute(
-                    'INSERT INTO employers (id, parentid, name, officeid) '
-                    'VALUES (%(id)s, %(ParentId)s, %(Name)s, %(OfficeId)s)',
+                    'INSERT INTO employers (id, parentid, name) '
+                    'VALUES (%(id)s, %(ParentId)s, %(Name)s)',
                     row
                 )
 

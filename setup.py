8ec08ea43b1fe7b6d6cd3d2e6e4aba6f00101f00
Mikhail Logachev <miha.loga@gmail.com>
2018-10-28 13:54:18 +0300

    use requirement from requirement.txt

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
+import os
+
 from setuptools import setup, find_packages
+
+
+def install_required():
+    with open(os.path.join(os.path.dirname(__file__), 'requirement.txt'), 'r') as f:
+        return list(f.readlines())
+
 
 setup(
     name='company-cli-tool',
     version='0.1',
     packages=find_packages(),
-    install_requires=[
-        'Click',
-        'psycopg2',
-    ],
+    install_requires=install_required(),
     entry_points='''
         [console_scripts]
         com-cli-tool=company_cli_tool.run:do

7132203486f8d07c7e8d90e4ac885078cb6bf9ee
Mikhail Logachev <miha.loga@gmail.com>
2018-10-28 13:31:20 +0300

    add options to office command

--- run.py
+++ run.py
@@ -9,8 +9,8 @@ from get_office import get_office
 
 @click.command('import')
 @click.argument('json_file')
-@click.option('--host', '-h', default='localhost', help="Postgres host")
-@click.option('--port', '-p', default='5432', help="Postgres port")
+@click.option('--host', '-h', default='localhost', help='Postgres host')
+@click.option('--port', '-p', default='5432', help='Postgres port')
 @click.option('--database', '-d', default='company', help='Postgres database name')
 @click.option('--user', '-u', default='postgres', help='Postgres user')
 @click.option('--password', '-w', default='', help='Postgres user password')
@@ -21,10 +21,13 @@ def run_import(json_file, **kwargs):
 
 @click.command('office')
 @click.argument('user_id')
-@click.option('--db', default='company', help='Postgres database name')
-@click.option('--user', default='postgres', help='Postgres user')
-def run_get_office(user_id, db, user):
-    print(get_office(int(user_id)))
+@click.option('--host', '-h', default='localhost', help='Postgres host')
+@click.option('--port', '-p', default='5432', help='Postgres port')
+@click.option('--database', '-d', default='company', help='Postgres database name')
+@click.option('--user', '-u', default='postgres', help='Postgres user')
+@click.option('--password', '-w', default='', help='Postgres user password')
+def run_get_office(user_id, **kwargs):
+    print(get_office(int(user_id), **kwargs))
 
 
 @click.group('do')
